a866c8f8706bd388a81cea6233d6837c5496905f
Rafael-Bira <rafaelmichelinsgmail.com>
2023-06-04 04:07:01 -0300

    Modified script.js, feedbacks.html and index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,16 @@
+// Adds a comment on feedbacks.html
 function addComment() {
     let getName = document.getElementById("name").value;
     let getPlace = document.getElementById("place").value;
     let getComment = document.getElementById("comment").value;
-    
+    const date = currentDate();
+    let newComment = document.createElement("p");
+    newComment.innerHTML = `<strong>${getName}</strong> <em>from</em> <strong>${getPlace}</strong> <em>said on</em> ${date}<br>&emsp;${getComment}`;
+    document.getElementById("comments").appendChild(newComment);
+}
+
+// Gets the current date 
+function currentDate() {
     let date = new Date();
     let month = date.getMonth()+1;
     let day = date.getDate();
@@ -11,8 +19,5 @@ function addComment() {
     if(hours < 10) { hours = "0" + hours};
     let minutes = date.getMinutes();
     if(minutes < 10) { minutes = "0" + minutes};
-
-    let newComment = document.createElement("p");
-    newComment.innerHTML = `<strong>${getName}</strong> <em>from</em> <strong>${getPlace}</strong> <em>said on</em> ${month}/${day}/${year}, ${hours}:${minutes}<br>&emsp;${getComment}`;
-    document.getElementById("comments").appendChild(newComment);
+    return `${month}/${day}/${year}, ${hours}:${minutes}`;
 }
